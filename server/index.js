92928425f4246ce713cfbae6cf517fb81b796a3f
techie-mohit <mohitkeshari2000@gmail.com>
2025-08-16 16:04:10 +0530

    stripe

--- server/index.js
+++ server/index.js
@@ -32,7 +32,7 @@ app.post('/api/stripe/webhook',
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    origin: "http://localhost:5173",    
+    origin: "https://learnify-uy89.onrender.com",    
     credentials: true,
 }))
 
