ee16cfda7192f49027f6b5ec5cbe115dd50c9bc3
techie-mohit <mohitkeshari2000@gmail.com>
2025-08-17 15:05:04 +0530

    deploy

--- server/utils/generateToken.js
+++ server/utils/generateToken.js
@@ -6,7 +6,8 @@ const generateToken = (res, user, message) => {
     return res.status(200).cookie(
         "token", token, 
         { httpOnly: true,                   // it prevent to access token via client side javascript
-            sameSite: 'strict',               // it prevent to send token to another site
+            sameSite: 'none',    
+            secure: true,                     // it prevent to send token to another site
             maxAge: 24 * 60 * 60 * 1000        // for 1 day
         }).json({
             success: true,
