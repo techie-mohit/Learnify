6d225a406e32ea1ae639bcaf8eab5509179af502
techie-mohit <mohitkeshari2000@gmail.com>
2025-08-17 15:00:33 +0530

    deploy

--- server/index.js
+++ server/index.js
@@ -31,10 +31,7 @@ app.use(cors({
     credentials: true,
 }))
 
-app.use(express.static(path.join(__dirname, 'client/dist')));
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
-})
+
 
 
 app.get("/home", (_, res)=>{
